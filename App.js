07544eebda0e1d960f91d2d1055ee3e065e05fd1
RomanVasilyev <roman.m.vasilyev@gmail.com>
2021-01-06 18:33:23 +0300

    First change - rendered text

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>It's a repo rating app.</Text>
       <StatusBar style="auto" />
     </View>
   );
